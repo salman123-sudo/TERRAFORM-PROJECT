7f88e76dee447efb41965832c44e7298093bf2d5
Salman Dileepkhan <salman120tg@gmail.com>
2024-12-18 12:58:11 +0530

    Add files via upload

---
+++ Keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNrGfJmhu8743NEy4mqkcasKoyobASENzlyjvyGhNJt salma@LAPTOP-VOPR6LA3"
+}
